245046d57223bd1f2da55364be1b3e892eb14b63
Rich-Harris <richard.a.harris@gmail.com>
2015-07-13 08:06:45 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var path = require( 'path' );
+var r = require( 'rollup-babel' );
+
+module.exports = function rollup ( inputdir, outputdir, options ) {
+	if ( !options.entry || !options.dest ) {
+		throw new Error( 'You must supply `entry` and `dest` options' );
+	}
+
+	options.entry = path.resolve( inputdir, options.entry );
+	options.dest = path.resolve( outputdir, options.dest );
+
+	return r.rollup( options ).then( function ( bundle ) {
+		return bundle.write( options );
+	});
+};
